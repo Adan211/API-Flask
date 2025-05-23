c49b2041c341af8a5488c7aac3f59fc5008cc78e
Adan211 <74257704+Adan211@users.noreply.github.com>
2025-05-22 23:57:53 -0500

    cuarto commit

--- app.py
+++ app.py
@@ -68,5 +68,7 @@ def reporte_ventas():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))  # Render define PORT, si no, usa 5000 por defecto
+    app.run(debug=True, host="0.0.0.0", port=port, threaded=True)
